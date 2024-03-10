c3e926d9c7b3d1043be81fa1ea7608bf9983ab29
SrSt0rm1 <142186365+SrSt0rm1@users.noreply.github.com>
2024-03-10 14:42:53 -0500

    Update DocumentoClonable.java

--- Patrones/Prototype/DocumentoClonable.java
+++ Patrones/Prototype/DocumentoClonable.java
@@ -1,5 +1,13 @@
 package Prototype;
 
+/**
+ * Clase que ejemplifica el uso del patrón prototype
+ * 
+ * @author Helen Giraldo, Juan Esteban Maya y Miguel Vargas
+ * @since 2024 - 03
+ *        Licencia GNU/GPL V3.0
+ */
+
 public interface DocumentoClonable {
     Documento clonar();
 }
